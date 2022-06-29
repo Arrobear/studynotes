e62895b6867002e8f7d6b9b914fe94beb5b95d06
Arrobear <2392126329@qq.com>
2022-06-30 00:01:06 +0800

    6_29_1

---
+++ 函数的嵌套调用和链式访问.cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+//int main()
+//{
+//	cout << "hello world!!" << endl;
+//}
+
+
+//写一个函数，每调用一次这个函数，就会将num的值增加1
+
+//void Add(int* p)
+//{
+//	(*p)++;
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	Add(&num);
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+
+//函数的嵌套调用和链式访问
+//void text3()
+//{
+//	printf("hehe\n";)
+//}
+//int text2()
+//{
+//	text3();
+//	return 0;
+//}
+//int main()
+//{
+//	text2();
+//	return 0;
+//}
+
+//链式访问：把一个函数的返回值作为另一个函数的参数
+//#include<string.h>
+//int main()
+//{
+//	//int len = strlen("abc");
+//	//printf("%d\n", len);
+//	//printf("%d\n", strlen("abc"))
+//	
+//	//char arr1[20] = { 0 };
+//	//char arr2[] = "bit";
+//	//strcpy(arr1, arr2);
+//
+//	//pritnf("%s\n", strcpy(arr1, arr2));
+//	
+//	printf("%d", printf("%d", printf("%d", 43)));
+//	return 0;
+//}
