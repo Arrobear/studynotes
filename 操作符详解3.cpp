4c13d35f57fc841eb64038f803dfd45ba513106f
Arrobear <2392126329@qq.com>
2022-07-04 22:11:17 +0800

    7_4

---
+++ 操作符详解3.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+//struct book
+//{
+//	char name[20];
+//	char id[20];
+//	int price;
+//};
+//int main()
+//{
+//	//结构成员访问操作符
+//	// .
+//	// ->
+//	// 结构体
+//	// 书：书名、书号、出版社、作者
+//	struct book a = { "c++","c202011010030",55 };
+//	book* pb = &a;
+//	printf("书名：%s\n", (*pb).name);
+//	printf("书号：%s\n", pb->id);
+//	//printf("书名：%s\n", a.name);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a + a * b;
+//	return 0;
+//}
+
