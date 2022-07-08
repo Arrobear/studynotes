abf6257d5074646ce9376862665ae738c4964ebb
Arrobear <2392126329@qq.com>
2022-07-08 22:18:46 +0800

    7_8

---
+++ 调试技巧2.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+//void text2()
+//{
+//	printf("hello world!!");
+//}
+//void text1()
+//{
+//	text2();
+//}
+//int main()
+//{
+//	text1();
+//	return 0;
+//}
+//调用堆栈：反馈函数调用逻辑
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n)
+//		int i = 0;
+//	int ret = 1;
+//	for (i = 0; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
