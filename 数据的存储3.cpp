e1ed352d2075a9f71e0537f01d047ffb1b6720d1
Arrobear <2392126329@qq.com>
2022-07-16 22:49:41 +0800

    7_15

---
+++ 数据的存储3.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//	}
+//	return 0;
+//}
+
+#include<string.h>
+int main()
+{
+	char a[1000];
+	int i;
+	for (i = 0; i < 1000; i++)
+	{
+		a[i] = -1 - i;
+	}
+	printf("%d", strlen(a));
+	return 0;
+}
